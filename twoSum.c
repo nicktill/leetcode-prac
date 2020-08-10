bbd4b93767a689782753bf1010020f6db9d51ec2
Nicholas Tillmann <57879193+NickTill@users.noreply.github.com>
2020-08-10 03:29:13 -0400

    Create twoSum.c

---
+++ twoSum.c
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int low = 0, high = numbers.size() - 1;
+        while (low < high) {
+            int sum = numbers[low] + numbers[high];
+            if (sum == target)
+                return {low + 1, high + 1};
+            else if (sum < target)
+                ++low;
+            else
+                --high;
+        }
+        return {-1, -1};
+    }
+};
