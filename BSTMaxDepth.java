f3abc6408d3f580f20e745b5d22e077c4158752e
Nicholas Tillmann <57879193+NickTill@users.noreply.github.com>
2020-08-10 03:30:46 -0400

    Create BSTMaxDepth.java

---
+++ BSTMaxDepth.java
@@ -0,0 +1,11 @@
+class Solution {
+  public int maxDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    } else {
+      int left_height = maxDepth(root.left);
+      int right_height = maxDepth(root.right);
+      return java.lang.Math.max(left_height, right_height) + 1;
+    }
+  }
+}
