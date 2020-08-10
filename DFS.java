e85208a306c6df66e1cb8e0de02de337e5e363f8
Nicholas Tillmann <57879193+NickTill@users.noreply.github.com>
2020-08-10 03:16:47 -0400

    Create DFS.java

---
+++ DFS.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Program {
+  static class Node {
+    String name;
+    List<Node> children = new ArrayList<Node>();
+
+    public Node(String name) {
+      this.name = name;
+    }
+
+    public List<String> depthFirstSearch(List<String> array) {
+			array.add(this.name);
+			for (int i = 0; i < children.size(); i++) {
+				children.get(i).depthFirstSearch(array);
+			}
+      return array;
+    }
+
+    public Node addChild(String name) {
+      Node child = new Node(name);
+      children.add(child);
+      return this;
+    }
+  }
+}
