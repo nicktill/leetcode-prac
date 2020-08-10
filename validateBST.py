8c8a168ad70f91eb892d22d536d83acabc7bcdbd
Nicholas Tillmann <57879193+NickTill@users.noreply.github.com>
2020-08-10 03:32:08 -0400

    Create validateBST.py

---
+++ validateBST.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def helper(node, lower = float('-inf'), upper = float('inf')):
+            if not node:
+                return True
+            
+            val = node.val
+            if val <= lower or val >= upper:
+                return False
+
+            if not helper(node.right, val, upper):
+                return False
+            if not helper(node.left, lower, val):
+                return False
+            return True
+
+        return helper(root)
