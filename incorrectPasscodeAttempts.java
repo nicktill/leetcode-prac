8605fc6a7d547eab3609b5cf08189969907ffe83
Nicholas Tillmann <nit36@pitt.edu>
2020-09-21 16:45:43 -0400

    Update incorrectPasscodeAttempts.java

--- incorrectPasscodeAttempts.java
+++ incorrectPasscodeAttempts.java
@@ -1,3 +1,7 @@
+//Opening the Dropbox mobile app on the VIU's tablet requires a four-digit passcode. To ensure the confidentiality of the stored information, the device is locked out of Dropbox after 10 consecutive failed passcode attempts. We need to implement a function that, given an array of attempts made throughout the day and the correct passcode, checks to see if the device should be locked, i.e. 10 or more consecutive failed passcode attempts were made.
+
+//Nicohlas Tillmann, CodeSignal Solution, 
+
 boolean incorrectPasscodeAttempts(String passcode, String[] attempts) {
     int wrongAttempts = 0;
     for (int i = 0; i < attempts.length; i++) {
