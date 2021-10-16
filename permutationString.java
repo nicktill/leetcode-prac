5df4823480ae3711f117035e3065c7af12d572cc
Nicholas Tillmann <nit36@pitt.edu>
2021-10-16 18:29:20 -0400

    adding LC questions

---
+++ permutationString.java
@@ -0,0 +1,17 @@
+public boolean checkInclusion(String s1, String s2) {
+    s1 = sort(s1);
+    for (int i = 0; i <= s2.length() - s1.length(); i++) {
+       if (s1.equals(sort(s2.substring(i, i + s1.length()))))
+            return true;
+    }
+    return false;
+}
+
+public String sort(String s) {
+    //cant use arrays.sort on a normal string
+    //so create a character array sort it
+    //and then pass it back as a string
+    char[] t = s.toCharArray();
+    Arrays.sort(t);
+    return new String(t);
+}
