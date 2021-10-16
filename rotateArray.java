948012b0aefa493629d07c9d5f2af062676d5c53
Nicholas Tillmann <nit36@pitt.edu>
2021-10-16 19:33:19 -0400

    LC

--- rotateArray.java
+++ rotateArray.java
@@ -1,7 +1,8 @@
 public void rotate(int[] nums, int k) {
     int[] a = new int[nums.length];
     for (int i = 0; i < nums.length; i++) {
-      a[(i + k) % nums.length] = nums[i];
+      a[(i + k) % nums.length] = nums[i]; //when the index allocated is higher than nums/length, we will get the overflow value
+      //which is equivilent to the shifted position
     }
       
     for(int i = 0; i < nums.length; i++){
