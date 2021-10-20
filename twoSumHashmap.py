01bb4f83450efae2dd89019ea65eba999d5db0a0
Nicholas Tillmann <nit36@pitt.edu>
2021-10-20 16:08:23 -0400

    LC

---
+++ twoSumHashmap.py
@@ -0,0 +1,7 @@
+ def twoSum(self, nums, target):
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
