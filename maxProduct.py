75e3c05424b4ed49b49c5007a94115e2a402e3bb
Nicholas Tillmann <nit36@pitt.edu>
2022-04-08 18:11:13 -0400

    leetcode grind baby

---
+++ maxProduct.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        #base check
+        if len(nums) == 0:
+            return 0
+        
+        currMax = nums[0] #grab first value of array
+        currMin = nums[0]
+        result = currMax 
+        
+        for i in range(1, len(nums)):
+            curr = nums[i] #grab second index
+            currMax = max(curr, currMax*curr, currMin*curr)
+            currMin = min(curr, currMax *curr, currMin*curr)
+            result = max(currMax, result)
+            
+        return result
