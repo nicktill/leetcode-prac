e9d79550c4b3304110716994557cc3b6efe485a7
Nicholas Tillmann <nit36@pitt.edu>
2022-04-14 21:11:21 -0400

    completing highest frequency leetcode medium from fb

--- minRemoveToMakeValid.py
+++ minRemoveToMakeValid.py
@@ -1,15 +1,16 @@
-class Solution(object):
-    def minRemoveToMakeValid(self, s):
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
         stack = []
-        s = list(s)
+        s = list(s) #turn string into list so we can iterate through characters
         for i in range(len(s)):
             if s[i] == '(':
-                stack.append(i)
+                stack.append(s[i])
             elif s[i] == ')':
-                if stack:
-                    stack.pop()
-                else:
-                    s[i] = "" #removing extra paranthesis
-        for x in stack:   
-            s[x] = ""
-        return "".join(s)
+                if stack: #if stack means that theres a '(' paranthesis added to stack
+                    stack.pop() #therefore pop since since found the end paranthesis ')'
+                else: #otherwise we have a ')' before a '(' and need to remove it from list
+                    s[i] = ""
+    
+        return "".join(s) #we need to do this to return the list of characters back into a string
+       
+                    
