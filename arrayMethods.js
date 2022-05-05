e126a1936d65775b190ba7e83d61407a779a3037
Nicholas Tillmann <ntillmann1439@gmail.com>
2022-05-05 17:27:04 -0400

    updates

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,17 +1,21 @@
-
-//AlgoExpert, Front-End Expert 
 Array.prototype.myMap = function (callback) {
     const list = [];
     for (let i = 0; i < this.length(); i++) {
         let value = callback(this[i], i, this)
         list.push(value);
     }
-}
+};
 
 Array.prototype.myFilter = function (callback) {
-    // Write your code here.
-}
+    const list = [];
+    for (let i = 0; i < this.length(); i++) {
+        if (callback(this[i], i, this) === true) {
+            list.push(this[i])
+        }
+    }
+    return list;
+};
 
 Array.prototype.myReduce = function (callback, initialValue) {
     // Write your code here.
-}
+};
