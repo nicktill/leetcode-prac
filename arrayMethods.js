2e2dd3be65f13263f4f267d683ec32c9009bba75
Nicholas Tillmann <ntillmann1439@gmail.com>
2022-05-05 17:37:07 -0400

    arrayMethods complete

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,21 +1,30 @@
 Array.prototype.myMap = function (callback) {
     const list = [];
-    for (let i = 0; i < this.length(); i++) {
-        let value = callback(this[i], i, this)
-        list.push(value);
+    for (let i = 0; i < this.length; i++) {
+        list.push(callback(this[i], i, this));
     }
-};
+    return list;
+}
 
 Array.prototype.myFilter = function (callback) {
     const list = [];
-    for (let i = 0; i < this.length(); i++) {
+    for (let i = 0; i < this.length; i++) {
         if (callback(this[i], i, this) === true) {
-            list.push(this[i])
+            list.push(this[i]);
         }
     }
     return list;
 };
 
 Array.prototype.myReduce = function (callback, initialValue) {
-    // Write your code here.
-};
+    let accummulator = initialValue;
+    for (let i = 0; i < this.length; i++) {
+        if (i === 0 && initialValue === undefined) {
+            accummulator = this[i];
+        }
+        else {
+            accummulator = callback(accummulator, this[i], i, this)
+        }
+    }
+    return accummulator;
+}
